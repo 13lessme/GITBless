693d2d03e19fff1084b403ed729d887ce26c1f50
13lessme <vldikk1@mail.ru>
2020-06-19 13:41:17 +0300

    Exemple Project

---
+++ app/src/main/java/com/example/blessnews/DetailActivity.kt
@@ -0,0 +1,24 @@
+package com.example.blessnews
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
+
+class DetailActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail)
+
+        val item = intent.getParcelableExtra<ItemOfList>("OBJECT_INTENT")
+
+        val imgSrc = findViewById<ImageView>(R.id._imageDetail)
+        val imgTitle = findViewById<TextView>(R.id._imageTitle)
+        val imgDesc = findViewById<TextView>(R.id._imageDesc)
+
+        imgSrc.setImageResource(item.imageSrc)
+        imgTitle.text = item.imageTitle
+        imgDesc.text = item.imageDesc
+    }
+}
